31659fe388c23b7d28fcaf7100e34e5023dcfed6
Queen506 <99583540+Queen506@users.noreply.github.com>
2024-02-06 02:39:48 +0700

    App.java

--- ex7/question1/App.java
+++ ex7/question1/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        IImage myImage = new Image("test.jpg");
+        IImage myImage = new ImageProxy("test.jpg");
         //do something
         myImage.display();        
     }
